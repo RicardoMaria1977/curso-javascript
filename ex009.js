fddff1946d1b35b11c26c0c3c661c5c2cb6ee927
RicardoMaria1977 <roliveiramaria@gmail.com>
2019-10-21 20:47:56 -0300

    meu primeiro projeto no git

---
+++ ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log (`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`Você é Brasileiro que bosta kkkk`)
+} 
+else {
+    console.log (`Você e estrangeiro!!!`)
+}
